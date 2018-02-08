9e8cbcecf2c0aa3836b042144a1f366d6dc4f0f4
Leolin1965 <claudeylionel@gmail.com>
2018-02-08 09:10:57 +0100

    Modif routeur pour comment vide

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ require_once  'controler/frontend/frontend.php';
 if(isset($_GET['action'])AND ($_GET['action']=='post')AND (isset($_GET['id'])))
         {
       post();
-}elseif(isset($_GET['action'])AND ($_GET['action']=='addComment')AND (isset($_GET['id']))){
-    
+}elseif(isset($_GET['action'])AND ($_GET['action']=='addComment') AND (isset($_GET['id']))){
+    if(empty($_POST['author'])|| empty($_POST['comment'])){
+        post();
+    }else{
     ajoutComment();
-    
-  }
-        elseif(isset($_GET['action'])AND ($_GET['action']=='enableSignal')AND (isset($_GET['id'])))
+     }
+} elseif(isset($_GET['action'])AND ($_GET['action']=='enableSignal')AND (isset($_GET['id'])))
         {
             activeSignal();
          }
