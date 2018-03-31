6f188a900fd87c04988e23392b6b444ad562651e
Leolin1965 <claudeylionel@gmail.com>
2018-04-01 00:37:55 +0200

    Modification fonction getPost model - htaccess

--- model/commun/Manager.php
+++ model/commun/Manager.php
@@ -5,15 +5,10 @@ require_once("model/commun/Config.php");
 class Manager extends Config {
            
     protected function dbConnect() {
-        try {
+  
                $db = new \PDO("mysql:host=".self::DBHOST.";dbname=".self::DBNAME.";charset=".self::CHARSET,self::DBUSER,self::DBPASS);
                $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                return $db;
-              
-        }
-        catch (Exception $e)
-        {
-        echo '[Exception] : ', $e->getMessage();
-        }
-    }    
+    }
+        
 }
--- model/frontend/CommentManager.php
+++ model/frontend/CommentManager.php
@@ -27,8 +27,8 @@ class CommentManager extends Manager {
 
     public function getComment($commentId) {
         $db = $this->dbConnect();
-        $comment = $db->prepare('SELECT COMM_ID, COMM_PSEUDO, COMM_CONTEN, DATE_FORMAT(COMM_DATE, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE COMM_ID= ? ORDER BY comment_date DESC');
-        $comment->execute(array($commentId));
+        $comment = $db->prepare('SELECT COMM_ID, COMM_PSEUDO, COMM_CONTENU, DATE_FORMAT(COMM_DATE, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE COMM_ID= ? AND DISABLE= ? ORDER BY comment_date DESC');
+        $comment->execute(array($commentId,0));
         return $comment;
     }
 
