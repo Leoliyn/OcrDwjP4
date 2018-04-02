ebdccdb2ec57fb6454e9a7eaa09f70c935db0b9d
Leolin1965 <claudeylionel@gmail.com>
2018-04-02 18:59:11 +0200

    Rajout Title lien barre de navigation

--- view/backend/navbar.php
+++ view/backend/navbar.php
@@ -16,10 +16,10 @@
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
 
-                <li><a href='indexadmin.php?action=changePsswd'><i class="fa fa-user   fa-3x "></i></a></li>
-                <li><a href='indexadmin.php'><i class="fa fa-file-o   fa-3x "></i></a></li>
-                <li><a href='indexadmin.php?action=listBooks'><i class="fa fa-envira   fa-3x "></i></a></li>
-                <li><a href='index.php'><i class="fa fa-home   fa-3x "></i></a></li>
+                <li><a href='indexadmin.php?action=changePsswd'title="Changement Password"><i class="fa fa-user   fa-3x "></i></a></li>
+                <li><a href='indexadmin.php'title ="Gestion des chapitres"><i class="fa fa-file-o   fa-3x "></i></a></li>
+                <li><a href='indexadmin.php?action=listBooks'title ="Gestion des ouvrages"><i class="fa fa-envira   fa-3x "></i></a></li>
+                <li><a href='index.php'title ="Retour Frontend"><i class="fa fa-home   fa-3x "></i></a></li>
 
 
 
